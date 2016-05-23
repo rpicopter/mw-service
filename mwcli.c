16c915f509d27004ff63660ed1127594b0b09190
Gregory Dymarek <gregd72002@gmail.com>
2016-05-23 11:21:52 +0000

    mwcli: adding read option

--- mwcli.c
+++ mwcli.c
@@ -20,12 +20,14 @@ uint8_t data[MSP_MAX_SIZE];
 uint8_t data_len;
 
 uint8_t verbosity;
+int8_t mode = -1;
 
 struct S_MSG msg;
 
 void print_usage() {
-    printf("Usage: mwcli -b [block] -i [ID] -d [data]\n");
+    printf("Usage: mwcli -b [block] -i [ID] -d [data] -r\n");
 	printf("-h\thelp\n");
+    printf("use -d for writing data; -r for reading\n");
     printf("[block] 0 for outgoing, 1 for incoming\n");
     printf("[ID] message ID\n");
     printf("[data] space separated decimals (uint8_t), i.e. 182 24 53 3\n");
@@ -37,7 +39,7 @@ int set_defaults(int c, char **a) {
     msg_id = 0;
     data_len = 0;
     verbosity = 0b00000000;
-    while ((option = getopt(c, a,"hvb:i:d:")) != -1) {
+    while ((option = getopt(c, a,"hvb:i:d:r")) != -1) {
         switch (option)  {
             case 'b': block = atoi(optarg); break;
             case 'v': verbosity = 0xFF; break;
@@ -46,6 +48,10 @@ int set_defaults(int c, char **a) {
             	for (i=optind-1;i<c-1;i++) {
             		data[data_len++] = atoi(a[i]); 
             	}
+                mode = 0;
+            break;
+            case 'r': 
+                mode = 1;
             break;
             default: 
             	print_usage(); 
@@ -53,13 +59,15 @@ int set_defaults(int c, char **a) {
         }
     }
 
-	if (data_len && msg_id) return 0;
+	if ((mode==0) && data_len && msg_id) return 0;
+    if ((mode==1) && msg_id) return 0;
 	print_usage(); 
 	return -1;
 }
 
 int main (int argc, char **argv)
 {
+    uint8_t i;
 	if (set_defaults(argc,argv)) return -1;
 
 	dbg_init(verbosity);
@@ -69,11 +77,23 @@ int main (int argc, char **argv)
 		return -1; //initiate channel to mw-service
 	}
 
-	mspmsg_custom_serialize(&msg,msg_id,data,data_len);
+    if (mode==0) {  //write 
+	   mspmsg_custom_serialize(&msg,msg_id,data,data_len);
 
-	if (verbosity) printf("MsgID: %u, data length: %u\n",msg_id,data_len);
-	if (block==0) shm_put_outgoing(&msg); //send it to the service
-	else shm_put_incoming(&msg); //send it to the service
+	   if (verbosity) printf("Writing MsgID: %u, data length: %u\n",msg_id,data_len);
+	   if (block==0) shm_put_outgoing(&msg); //send it to the service
+	   else shm_put_incoming(&msg); //send it to the service
+    } else if (mode==1) {
+        if (verbosity) printf("Reading MsgID: %u\n",msg_id);
+        if (block==0) shm_get_outgoing(&msg,msg_id); //send it to the service
+        else shm_get_incoming(&msg,msg_id); //send it to the service
+        if (verbosity) printf("Length: %u Data: %s\n",msg.size,dbg_printHEX(msg.data,msg.size));
+        else {
+            for (i=0;i<msg.size;i++)
+                printf("%c",msg.data[i]);
+            printf("\n");
+        }
+    }
 
 	shm_client_end(); //close channel to mw-service
 
